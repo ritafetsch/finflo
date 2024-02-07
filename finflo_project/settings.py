fd89d779611fe932b3f04f9f3cd862a0882a23c5
ritafetsch <ritafetsch1@gmail.com>
2024-02-07 14:13:18 +0000

    Vercel config

--- finflo_project/settings.py
+++ finflo_project/settings.py
@@ -25,12 +25,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-z+8$u=-5mae)+#we++!p*dm*!=wh$025*&!mq$3arvrv9%=-$_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ritafetsch.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
