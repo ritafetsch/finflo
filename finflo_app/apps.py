3afb4d1a6b0be3a2f372b649befd6fe52bb7bcec
Rita Fetsch <ritamelanie5@gmail.com>
2025-04-09 13:51:52 +0100

    remove bug

--- finflo_app/apps.py
+++ finflo_app/apps.py
@@ -1,15 +1,4 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
-def create_default_currencies(sender, **kwargs):
-    from .models import Currency
-    if not Currency.objects.exists():
-        Currency.objects.create(
-            name='US Dollar',
-            code='USD',
-            symbol='$',
-            exchange_rate=1.0
-        )
 # Configure context processors file
 class FinfloAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
